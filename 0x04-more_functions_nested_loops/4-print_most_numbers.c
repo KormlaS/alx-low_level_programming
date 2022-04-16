965dce2cdb1828398ed9b77bced3f7fc01073c8e
KormlaS <kselasi41@gmail.com>
2022-04-15 23:48:43 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - Prints except 2 and 4.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 void print_most_numbers(void)
@@ -14,11 +14,12 @@ i = 0;
 
 for (i = 0; i < 10; i++)
 {
+
 if (i == 2 || i == 4)
 
 i++;
 
-_putchar (i + 0);
+_putchar (i + '0');
 
 }
 _putchar ('\n');
